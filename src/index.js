0cad83920f464aa7294994e0e00967fb9e9dc56f
luk-marshal <lukaszm987@o2.pl>
2024-02-23 18:44:30 +0100

    Small bug fix in Footer

--- src/index.js
+++ src/index.js
@@ -134,7 +134,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <Order closeHour={closeHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -148,8 +148,8 @@ function Order({ closeHour, openHour }) {
   return (
     <div className="order">
       <p>
-        We're open until from {openHour}:00 to {closeHour}:00. Come visit us or
-        order online.{" "}
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.{" "}
       </p>
       <button className="btn">Order</button>
     </div>
